f4f94c62acdeef850ca0f737056f44c0894ebf1d
Juan Jose Comellas <juanjo@comellas.org>
2019-05-03 10:17:11 -0300

    Bump version to 0.5.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AppConfig.Mixfile do
   def project do
     [
       app: :app_config,
-      version: "0.5.2",
+      version: "0.5.3",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
